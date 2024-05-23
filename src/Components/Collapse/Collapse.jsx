1efabf303658637a2b505c508c8c6354221f0f12
Nguyen Thê-viet <theviet.pham@gmail.com>
2024-05-23 15:23:08 +0200

    finition code + chgt méthode collapse ( add eventlistener)

--- src/Components/Collapse/Collapse.jsx
+++ src/Components/Collapse/Collapse.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Collapse.scss';
-import { useState } from 'react';
 import collapseArrow from '../../Assets/Pictures/arrow_up.png';
 
 function Collapse({ title, content }) {
-
 	const [active, setActive] = useState(false);
+	const collapseRef = useRef(null);
 
 	const handleToggle = () => {
-		setActive(!active);
+		setActive(prevActive => !prevActive);
 	};
+
+	useEffect(() => {
+		const element = collapseRef.current;
+		if (element) {
+			element.addEventListener('click', handleToggle);
+		}
+		return () => {
+			if (element) {
+				element.removeEventListener('click', handleToggle);
+			}
+		};
+	}, []);
 
 	return (
 		<div className={`collapse ${active && 'active'}`}>
 			<div className='collapse__title'>
 				{title}
 				<img
+					ref={collapseRef}
 					className="collapse__icon"
-					onClick={handleToggle}
 					src={collapseArrow}
 					alt='collapse arrow'
 				/>
 			</div>
-			<div className="collapse__content">{content}</div>
+			{active && <div className="collapse__content">{content}</div>}
 		</div>
 	);
 };
 
-
-export default Collapse
+export default Collapse;
